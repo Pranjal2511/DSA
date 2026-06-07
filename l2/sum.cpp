6d48bf964bdef8a569477d19b12170f1f41b47f1
pranjalgautam62-cmd <233206710+Pranjal2511@users.noreply.github.com>
2026-06-07 23:29:18 +0530

    pushing codes

---
+++ l2/sum.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int sum = a+b;
+    cout<<"Sum of a+b = "<<sum<<endl;
+    return 0;
+}
---
+++ l2/typecasting.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double price=100.99;
+    int newPrice=price;
+    cout << newPrice<<endl;
+    return 0;
+}
