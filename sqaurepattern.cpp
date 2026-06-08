881b6ecedd3a35bd05e9f80622f71fcafc7ccb4f
pranjalgautam62-cmd <233206710+Pranjal2511@users.noreply.github.com>
2026-06-08 17:02:54 +0530

    more practice

---
+++ sqaurepattern.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        // int sum =0;
+
+        for(int j=1;j<=n;j++){
+            cout<< "*"<<" ";
+            // sum=sum+1;
+            // break;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
